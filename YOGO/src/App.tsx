7d068866b7edd68d1d9d5aaa4293f2aa2fd7f5a5
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-04-02 12:17:00 +0900

    chore: 필요 없는 코드 삭제

--- YOGO/src/App.tsx
+++ YOGO/src/App.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home, Detail } from 'screens';
-import { useTimeZone } from 'hooks';
 
 const Tab = createBottomTabNavigator();
 
 function App() {
-  console.log(useTimeZone());
   return (
     <NavigationContainer>
       <Tab.Navigator initialRouteName="Home">
