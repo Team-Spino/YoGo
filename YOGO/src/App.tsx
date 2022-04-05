73b660455449de4bc682a21508d7505d377c726d
IGhos-P <dndb3599@gmail.com>
2022-04-05 19:44:59 +0900

    feat : svg 사용가능

--- YOGO/src/App.tsx
+++ YOGO/src/App.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home, Detail } from 'screens';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Tab = createBottomTabNavigator();
+
 
 function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator initialRouteName="Home">
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Search" component={Detail} />
+        <Tab.Screen name="Home" component={Home} options={{ title: '홈'}}/>
+        <Tab.Screen name="Search" component={Detail} options={{ title: '홈',}} />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- YOGO/src/screens/Home/index.tsx
+++ YOGO/src/screens/Home/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import * as S from './style';
+import IconHome from '../../assets/icons/IconHome';
+
 
 export function Home() {
   return (
     <S.Container>
+    <IconHome />
       <S.Title>Home</S.Title>
     </S.Container>
   );
