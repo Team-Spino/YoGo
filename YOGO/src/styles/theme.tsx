c9e50f9b599c9af37ff3ae59e219b33d57b1380f
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-04-08 00:23:27 +0900

    Merge branch 'develop' into revert-90-feature/SW/makeSvgfile

--- YOGO/src/styles/theme.tsx
+++ YOGO/src/styles/theme.tsx
@@ -1,6 +1,7 @@
 // styled.d.ts
 import 'styled-components/native';
 import { DefaultTheme } from 'styled-components/native';
+
 export const theme: DefaultTheme = {
   font: 'Noto Sans KR',
   colors: {
