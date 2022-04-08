c48a990b11b37b3b7e012cd9cc6df79d9598f8bf
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-04-08 16:21:21 +0900

    fix: floating 버튼 css 수정

--- YOGO/src/components/atoms/FloatingButton/style.tsx
+++ YOGO/src/components/atoms/FloatingButton/style.tsx
@@ -6,7 +6,7 @@ export const Wrapper = styled.View`
   right: 16px;
   width: 56px;
   height: 56px;
-  borderradius: 28px;
+  border-radius: 28px;
   // iOS 전용 그림자 설정
   shadowcolor: '#4d4d4d';
   shadowoffset: {
@@ -22,9 +22,9 @@ export const Wrapper = styled.View`
 export const Button = styled.View`
   width: 56px;
   height: 56px;
-  borderradius: 28px;
-  alignitems: center;
-  justifycontent: center;
-  backgroundcolor: ${props => props.theme.colors.blue};
+  border-radius: 28px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${props => props.theme.colors.blue};
   opacity: ${({ pressed }: { pressed: boolean }) => (pressed ? 0.55 : 1)};
 `;
