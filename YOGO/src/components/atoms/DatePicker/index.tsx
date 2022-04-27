4f2cb93190acf080080a9110734a6da73d2ba6ca
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-04-27 23:27:51 +0900

    chore: 사용하지 않는 파일 삭제

--- YOGO/src/components/atoms/DatePicker/index.tsx
+++ YOGO/src/components/atoms/DatePicker/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import DateTimePicker, {
   DateTimePickerEvent,
 } from '@react-native-community/datetimepicker';
--- YOGO/src/components/molecules/SelectTargetDate/style.tsx
+++ YOGO/src/components/molecules/SelectTargetDate/style.tsx
@@ -3,13 +3,6 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   margin-top : 20px;
   width: 90%; 
-  height: 65%;
+  flex: 1;
 `
 
-export const PressContainer = styled.TouchableOpacity`
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  align-items: center;
-  jsutify-content: space-between;
-` 
