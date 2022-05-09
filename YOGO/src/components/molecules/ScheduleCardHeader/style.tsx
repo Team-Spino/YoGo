f2493f57a3c6419efa80ca36f85f3ec7779b1c02
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-05-09 15:04:45 +0900

    fix: 스케줄 카드 레이아웃 수정

--- YOGO/src/components/molecules/ScheduleCardHeader/style.tsx
+++ YOGO/src/components/molecules/ScheduleCardHeader/style.tsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+  width: 100%;
+  flex: 1;
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   flex-direction: row;
 
