b15041dece9ea61eefdb545bc663c821069d5c2e
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-05-11 20:32:52 +0900

    feat: 모든 필드에 대해 저장한다면, 매 초마다 저장해야 하기 때문에 도시만 저장하도록 처리

--- YOGO/src/db/timezone.ts
+++ YOGO/src/db/timezone.ts
@@ -14,11 +14,7 @@ export const createTimezoneTable = async (db: SQLiteDatabase) => {
   const query = `CREATE TABLE IF NOT EXISTS timezone 
     (
       ID INTEGER PRIMARY KEY AUTOINCREMENT,
-      TIME TEXT NOT NULL,
-      DATE TEXT NOT NULL,
       CITY TEXT NOT NULL,
-      MERIDIEM TEXT NOT NULL,
-      TIMEDIFFERENCE TEXT NOT NULL
     );`;
 
   await db.executeSql(query);
