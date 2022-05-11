5863013ac7a735a037df774cbf18fe40662af6ea
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-05-11 21:26:58 +0900

    refactor: 필요 없는 fragment 삭제 & any 수정

--- YOGO/src/components/organisms/TimeZoneList/index.tsx
+++ YOGO/src/components/organisms/TimeZoneList/index.tsx
@@ -4,17 +4,14 @@ import { TimeZoneCard } from 'components';
 import { DUMMY_TIME_ZONE } from 'utils';
 import * as S from './style';
 
-export function TimeZoneList({cardList}: any) {
+export function TimeZoneList({ cardList }: { cardList: Array<string> }) {
   return (
-    <>
-      <S.Container>
+    <S.Container>
       <S.ScrollView>
         {cardList.map((location: string) => (
-          <TimeZoneCard key={uuid.v4()} location={location} />  
-        )
-        )}
-        </S.ScrollView>
-      </S.Container>
-    </>
+          <TimeZoneCard key={uuid.v4()} location={location} />
+        ))}
+      </S.ScrollView>
+    </S.Container>
   );
 }
--- YOGO/src/db/index.ts
+++ YOGO/src/db/index.ts
@@ -6,3 +6,24 @@ export {
   deleteTimezoneItem,
   dropTimezoneTable,
 } from 'db/timezone';
+
+//   const loadDataCallback = useCallback(async () => {
+//     try {
+//       const initTodos = [
+//         { id: 0, value: 'go to shop' },
+//         { id: 1, value: 'eat at least a one healthy foods' },
+//         { id: 2, value: 'Do some exercises' },
+//       ];
+//       const db = await getDBConnection();
+//       await createTable(db);
+//       const storedTodoItems = await getTodoItems(db);
+//       if (storedTodoItems.length) {
+//         setTodos(storedTodoItems);
+//       } else {
+//         await saveTodoItems(db, initTodos);
+//         setTodos(initTodos);
+//       }
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }, []);
