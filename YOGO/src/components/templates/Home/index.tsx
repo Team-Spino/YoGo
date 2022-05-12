68e3cca485bfbe3c2a65b92037df91876b686e12
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-05-12 12:04:16 +0900

    refactor: 코드 포멧 수정

--- YOGO/src/components/templates/Home/index.tsx
+++ YOGO/src/components/templates/Home/index.tsx
@@ -10,37 +10,43 @@ import * as S from './style';
 type Prop = NativeStackNavigationProp<RootStackParamList, 'HandleSchedule'>;
 
 export function Home({ navigation }: { navigation: Prop }) {
-
   const [selectedDay, setSelectedDay] = useState(dayjs().format('YYYY-MM-DD'));
 
-  const [data , setDate] = useState(DUMMY_DATA);
+  const [data, setDate] = useState(DUMMY_DATA);
 
-  const [markedDates , setMarkedDate] = useState({});
+  const [markedDates, setMarkedDate] = useState({});
 
   const onPress = () => {
     navigation.push('HandleSchedule', { title: 'Add' });
   };
 
-  const onDayPress = (day : string) => {
+  const onDayPress = (day: string) => {
     setSelectedDay(day);
   };
 
   useEffect(() => {
-    setDate(DUMMY_DATA.filter(({cur}) => cur.day === selectedDay));
+    setDate(DUMMY_DATA.filter(({ cur }) => cur.day === selectedDay));
     let markedDates = {};
-    DUMMY_DATA.forEach(({cur}) => { markedDates = {
-      ...markedDates,
-      ...{ [cur.day]: { marked: true } },
-    }})
+    DUMMY_DATA.forEach(({ cur }) => {
+      markedDates = {
+        ...markedDates,
+        ...{ [cur.day]: { marked: true } },
+      };
+    });
     setMarkedDate(markedDates);
   }, [selectedDay]);
 
   return (
     <S.Container>
-      <AgendaBox data={data} selectedDay={selectedDay} onDayPress={onDayPress} markedDates = {markedDates}/>
-        <FloatingButton onPress={onPress}>
-          <IconPlus />
-        </FloatingButton>
+      <AgendaBox
+        data={data}
+        selectedDay={selectedDay}
+        onDayPress={onDayPress}
+        markedDates={markedDates}
+      />
+      <FloatingButton onPress={onPress}>
+        <IconPlus />
+      </FloatingButton>
     </S.Container>
   );
 }
