69bcd5f217bea82ac612ab368cdb219c69d42211
IGhos-P <dndb3599@gmail.com>
2022-05-19 17:35:45 +0900

    refactor : 코드 정리

--- YOGO/src/components/templates/Home/index.tsx
+++ YOGO/src/components/templates/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState, useContext, useCallback } from 'react';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import dayjs from 'dayjs';
 import { FloatingButton, AgendaBox } from 'components';
@@ -38,12 +38,14 @@ export function Home({ navigation }: { navigation: Prop }) {
 
   const onEditTarget = async (item: IScheduleProps) => {
     navigation.push('HandleSchedule', { title: 'Edit', item });
+    markedDB();
   };
 
   const onDeleteTarget = async (id: number) => {
     setSchedules(schedules.filter(item => item.key !== id));
     const db = await connectDB();
     await deleteScheduleItem(db, id);
+    markedDB();
   };
 
   const initDB = async () => {
@@ -55,58 +57,64 @@ export function Home({ navigation }: { navigation: Prop }) {
       });
 
       const items = await getScheduleItems(db, dayOfWeek, selectedDay);
-
-      const dateAndDayOfWeek = await getDateAndDayOfWeek(db)
-      const {dateList, week} = dividDateAndDayOfWeek(dateAndDayOfWeek)
-      const weekList = makeWeekList(week)
-      makeMarkedDates([...dateList, ...weekList]);
       setSchedules(items);
     } catch (e) {
       console.error(e);
     }
   };
+  
+  const markedDB = async () => {
+    const db = await connectDB();
+    const dateAndDayOfWeek = await getDateAndDayOfWeek(db)
+    const {dateList, rowWeek} = dividDateAndDayOfWeek(dateAndDayOfWeek)
+    const weekList = makeWeekList(rowWeek)
+    makeMarkedDates([...dateList, ...weekList]);
+  }
 
-  const dividDateAndDayOfWeek = (dateAndDayOfWeek : any) => {
+  interface IDateAndDayOfWeek {
+    result : string
+  }
+
+  const dividDateAndDayOfWeek = (dateAndDayOfWeek : IDateAndDayOfWeek[]) => {
     const checkYYMMDD = /\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])/;
     const removeSpecial= /[\{\}\[\]\/?.;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi
     const dateList: string[] = []
     const dayOfWeekList: string[] = []
-
-    dateAndDayOfWeek.forEach((item: any) => {
+    dateAndDayOfWeek.forEach((item: IDateAndDayOfWeek) => {
       const isDate = checkYYMMDD.exec(item.result);
       if(isDate){
         return dateList.push(item.result)
       }
-      return item.result.replace(removeSpecial,'').split(',').map((el: any) => dayOfWeekList.push(el))
+      return item.result.replace(removeSpecial,'').split(',').map((el: string) => dayOfWeekList.push(el))
     })
-    const week = countDayOfWeek(dayOfWeekList)
-    return { dateList, week }
+    const rowWeek = countDayOfWeek(dayOfWeekList)
+    return { dateList, rowWeek }
   }
 
-  const countDayOfWeek = (dayOfWeekList : any) => {
-    return dayOfWeekList.reduce((accu, curr) => { 
+  const countDayOfWeek = (dayOfWeekList : string[]) => {
+    return dayOfWeekList.reduce((accu: any, curr: string) => { 
       accu[curr] = (accu[curr] || 0)+1; 
       return accu;
     }, {});
   }
 
-  const makeMarkedDates = (dateList : any) => {
+  const makeMarkedDates = useCallback((dateList : string[]) => {
     let markedDates = {};
-    dateList.forEach((day) => {
+    dateList.forEach((day: string) => {
       markedDates = {
         ...markedDates,
         ...{ [day]: { marked: true } },
       };
     });
     setMarkedDate(markedDates);
-  }
+  }, [])
 
-  const makeWeekList = (week : object) => {
+  const makeWeekList = (rowWeek : object) => {
     const weekLiteral = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     const today = dayjs(selectedDay).format('ddd');
     const oneDay = 86400
     let weekList = []
-    for (const [key, value] of Object.entries(week)) {
+    for (const [key, value] of Object.entries(rowWeek)) {
       let diff = weekLiteral.indexOf(key) - weekLiteral.indexOf(today)
       let diffTimeStamp = diff * oneDay
       let date = dayjs(selectedDay).add(diffTimeStamp, 'second').format('YYYY-MM-DD')
@@ -119,6 +127,7 @@ export function Home({ navigation }: { navigation: Prop }) {
 
   useEffect(() => {
     initDB();
+    markedDB();
     setPop(false);
   }, [selectedDay, setSelectedDay, isPoped, setPop]);
 
