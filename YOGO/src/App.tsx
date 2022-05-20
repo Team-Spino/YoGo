43ae7662ca58502c1acc4a3d380237992fcdd81f
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-05-21 00:08:04 +0900

    refactor: notification 허가로직 useNotification으로 분리

--- YOGO/src/App.tsx
+++ YOGO/src/App.tsx
@@ -1,73 +1,18 @@
 import React, { useEffect } from 'react';
-import { Alert, Linking } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import PushNotificationIOS from '@react-native-community/push-notification-ios';
 import { Main, HandleSchedule } from 'screens';
-import {
-  connectDB,
-  createAlarmPermissionTable,
-  inesertAlarmPermission,
-  getAlarmPermission,
-  updateAlarmPermission,
-  deleteAlarmPermission,
-} from 'db';
 import { theme } from 'styles/theme';
+import { useNotification } from 'hooks';
 import { PopProvider } from 'context';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-  const notificationLogic = async () => {
-    await PushNotificationIOS.requestPermissions();
-    PushNotificationIOS.setApplicationIconBadgeNumber(0);
-
-    PushNotificationIOS.checkPermissions(async info => {
-      const db = await connectDB();
-
-      await createAlarmPermissionTable(db);
-
-      const permission = await getAlarmPermission(db);
-
-      // 알람이 허가되었고, db에 반영되지 않았을 때
-      if (info.notificationCenter && !permission) {
-        await inesertAlarmPermission(db, 1);
-        return;
-      }
-
-      // 알람이 허가 되었고, db에 isAgree가 0일때 -> db에 업데이트
-      if (info.notificationCenter && !permission.IS_AGREE) {
-        await updateAlarmPermission(db, 1);
-      }
-
-      // 알람이 허가되었고, db에 active 되었을 때
-      if (info.notificationCenter && permission.IS_AGREE) return;
-
-      // 알람이 허가되지 않았고, db에 반영되지 않았을 때
-      if (!info.notificationCenter && !permission) {
-        Alert.alert('YOGO', 'Please use Notification', [
-          {
-            text: 'Cancel',
-            onPress: async () => {
-              await inesertAlarmPermission(db, 0);
-            },
-            style: 'cancel',
-          },
-          {
-            text: 'OK',
-            onPress: () => {
-              Linking.openSettings();
-            },
-          },
-        ]);
-
-        return;
-      }
-    });
-  };
+  const { handleNotificationPermission } = useNotification();
   useEffect(() => {
-    notificationLogic();
+    handleNotificationPermission();
   }, []);
 
   return (
--- YOGO/src/hooks/useNotification.tsx
+++ YOGO/src/hooks/useNotification.tsx
@@ -1,7 +1,16 @@
+import { Alert, Linking } from 'react-native';
 import PushNotification from 'react-native-push-notification';
 import PushNotificationIOS from '@react-native-community/push-notification-ios';
 import dayjs from 'dayjs';
 import uuid from 'react-native-uuid';
+import {
+  connectDB,
+  createAlarmPermissionTable,
+  inesertAlarmPermission,
+  getAlarmPermission,
+  updateAlarmPermission,
+  deleteAlarmPermission,
+} from 'db';
 import { IScheduleProps } from 'types';
 
 interface INotificationProps {
@@ -164,5 +173,58 @@ export function useNotification() {
     });
   };
 
-  return { makeNotification, deleteAllNotification, handleScheduleToggle };
+  const handleNotificationPermission = async () => {
+    await PushNotificationIOS.requestPermissions();
+
+    // 사용자 허가 체크
+    PushNotificationIOS.checkPermissions(async info => {
+      const db = await connectDB();
+
+      await createAlarmPermissionTable(db);
+
+      const permission = await getAlarmPermission(db);
+
+      // 알람이 허가되었고, db에 반영되지 않았을 때
+      if (info.notificationCenter && !permission) {
+        await inesertAlarmPermission(db, 1);
+        return;
+      }
+
+      // 알람이 허가 되었고, db에 isAgree가 0일때 -> db에 업데이트
+      if (info.notificationCenter && !permission.IS_AGREE) {
+        await updateAlarmPermission(db, 1);
+      }
+
+      // 알람이 허가되었고, db에 active 되었을 때
+      if (info.notificationCenter && permission.IS_AGREE) return;
+
+      // 알람이 허가되지 않았고, db에 반영되지 않았을 때
+      if (!info.notificationCenter && !permission) {
+        Alert.alert('YOGO', 'Please use Notification', [
+          {
+            text: 'Cancel',
+            onPress: async () => {
+              await inesertAlarmPermission(db, 0);
+            },
+            style: 'cancel',
+          },
+          {
+            text: 'OK',
+            onPress: () => {
+              Linking.openSettings();
+            },
+          },
+        ]);
+
+        return;
+      }
+    });
+  };
+
+  return {
+    makeNotification,
+    deleteAllNotification,
+    handleScheduleToggle,
+    handleNotificationPermission,
+  };
 }
