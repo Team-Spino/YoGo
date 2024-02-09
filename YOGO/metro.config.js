573f92ebce7f9dcf1bb920e8aa0cf0a903180060
SangKwon_Yeum <kunshup2000@gmail.com>
2024-02-10 01:43:54 +0900

    Revert "To RN 0.73.3 (#158)" (#159)
    This reverts commit 742b92daed90dd7574a63807cfae670121860046.

---
+++ YOGO/metro.config.js
@@ -0,0 +1,36 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+// 수정전
+// module.exports = {
+//   transformer: {
+//     getTransformOptions: async () => ({
+//       transform: {
+//         experimentalImportSupport: false,
+//         inlineRequires: true,
+//       },
+//     }),
+//   },
+// };
+
+// 수정후
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
