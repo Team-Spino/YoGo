e436fd1c302b1cfe346fc22e778a2731a969c07b
Yummy-sk <kunshup2000@gmail.com>
2024-02-10 17:33:00 +0900

    fix: async strorage -> mmkv

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,2 +1,2 @@
 export { PopContext, PopProvider } from 'context/popState';
-export { IsFirstContext, IsFirstProvider } from 'context/firstState';
+export { FirstLaunchContext, FirstLaunchProvider } from 'context/firstLaunch';
